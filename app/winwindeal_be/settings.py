d67b37b5fd3a9d049edb761cdc121c61e21c5178
박성민 <sungmini@bagseongmin-ui-MacBookAir.local>
2024-12-20 14:27:53 +0900

    setting file 수정9

--- app/winwindeal_be/settings.py
+++ app/winwindeal_be/settings.py
@@ -128,6 +128,10 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://192.168.0.239:3000",
+    "http://winwindeal.shop",
+    "https://winwindeal.shop",
+    "http://api.winwindeal.shop",
+    "https://api.winwindeal.shop",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
