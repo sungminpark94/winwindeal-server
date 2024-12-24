1855919d429c163c352f813805dac0f0197d6fc2
박성민 <sungmini@bagseongmin-ui-MacBookAir.local>
2024-12-24 19:26:41 +0900

    서버 연결 실패개선2

--- app/car/urls.py
+++ app/car/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('price/search', views.search_car_price_view),
-    path('data', views.car_data),
+    path('price/search/', views.search_car_price_view),
+    path('data/', views.car_data),
  # 새로 추가되는 URL 패턴
     path('update/', views.update_manual_car_info, name='update_manual_car_info'),
-    path('info/save', views.save_car_info, name='save_car_info'),  # 이 줄 추가
+    path('info/save/', views.save_car_info, name='save_car_info'),  # 이 줄 추가
 ]
 
 # from django.urls import path
